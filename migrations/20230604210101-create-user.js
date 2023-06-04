5677dd3a0e0b0a4ca9812c6882aaacf49a865066
Musthafa Faishal <musthafafaishal@gmail.com>
2023-06-05 05:09:27 +0700

    build: add migrations

---
+++ migrations/20230604210101-create-user.js
@@ -0,0 +1,47 @@
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        primaryKey: true,
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      username: {
+        type: Sequelize.STRING,
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      phoneNumber: {
+        type: Sequelize.STRING,
+      },
+      address: {
+        type: Sequelize.STRING,
+      },
+      gender: {
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Users');
+  },
+};
---
+++ migrations/20230604210257-create-survey.js
@@ -0,0 +1,50 @@
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Surveys', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.STRING,
+      },
+      userId: {
+        type: Sequelize.STRING,
+        references: {
+          model: {
+            tableName: 'users',
+            key: 'id',
+          },
+        },
+      },
+      moodId: {
+        type: Sequelize.STRING,
+        references: {
+          model: {
+            tableName: 'moods',
+            key: 'id',
+          },
+        },
+      },
+      budget: {
+        type: Sequelize.STRING,
+      },
+      travelDistance: {
+        type: Sequelize.STRING,
+      },
+      destinationCity: {
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Surveys');
+  },
+};

aaedda8f621b4b71d589b70bde2820025da71a5f
Musthafa Faishal <musthafafaishal@gmail.com>
2023-06-07 03:28:52 +0700

    refactor: code refactor

--- config/config.js
+++ config/config.js
@@ -19,10 +19,13 @@ module.exports = {
     username: process.env.MYSQL_USERNAME,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE,
-    host: '/cloudsql/viveteam:asia-southeast2:vive-app-sql-instance',
+    host: process.env.MYSQL_HOST,
     dialect: process.env.MYSQL_DIALECT,
-    dialectOptions: {
-      socketPath: '/cloudsql/viveteam:asia-southeast2:vive-app-sql-instance',
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
     },
   },
 };

34a8ca6d523c34eff7deaac5b03c3b6a5ccb7e66
Musthafa Faishal <musthafafaishal@gmail.com>
2023-06-07 12:50:04 +0700

    refactor: code refactor and remove unused code

--- src/server.js
+++ src/server.js
@@ -1,40 +1,33 @@
 require('dotenv').config();
 
 const express = require('express');
-const sequelize = require('./config/database');
 const userRoutes = require('./api/v1/users/routes');
 const authenticationRoutes = require('./api/v1/authentications/routes');
 
 const app = express();
 
 const init = async () => {
-  try {
-    await sequelize.authenticate();
+  app.use(express.json());
+  app.use('/api/v1/users', userRoutes);
+  app.use('/api/v1/authentications', authenticationRoutes);
 
-    app.use(express.json());
-    app.use('/api/v1/users', userRoutes);
-    app.use('/api/v1/authentications', authenticationRoutes);
+  app.all('*', (req, res, next) => {
+    const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+    err.status = 404;
+    err.statusCode = 404;
+    next(err);
+  });
 
-    app.all('*', (req, res, next) => {
-      const err = new Error(`Can't find ${req.originalUrl} on this server!`);
-      err.status = 404;
-      err.statusCode = 404;
-      next(err);
+  app.use((err, req, res, next) => {
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || 'error';
+    res.status(err.statusCode).json({
+      error: true,
+      message: err.message,
     });
+  });
 
-    app.use((err, req, res, next) => {
-      err.statusCode = err.statusCode || 500;
-      err.status = err.status || 'error';
-      res.status(err.statusCode).json({
-        error: true,
-        message: err.message,
-      });
-    });
-
-    app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
-  } catch (error) {
-    console.error(`Unable to run server: ${error}`);
-  }
+  app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
 };
 
 init();

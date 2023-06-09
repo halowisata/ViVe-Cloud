7246d080842ae06f62abbc668a252aec235191f4
Musthafa Faishal <musthafafaishal@gmail.com>
2023-06-09 20:46:15 +0700

    refactor: register new bookmark endpoint

--- src/api/v1/index.js
+++ src/api/v1/index.js
@@ -5,6 +5,7 @@ const authenticationRoutes = require('./authentications/routes');
 const moodRoutes = require('./moods/routes');
 const surveyRoutes = require('./surveys/routes');
 const touristAttractionRoutes = require('./tourist_attractions/routes');
+const userTouristAttractionRoutes = require('./user_tourist_attractions/routes');
 
 const router = express.Router();
 
@@ -13,5 +14,6 @@ router.use('/authentications', authenticationRoutes);
 router.use('/moods', moodRoutes);
 router.use('/surveys', surveyRoutes);
 router.use('/tourist-attractions', touristAttractionRoutes);
+router.use('/user-tourist-attractions', userTouristAttractionRoutes);
 
 module.exports = router;
